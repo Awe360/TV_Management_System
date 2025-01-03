4ef0e13be68840b246a3876828218d6164720110
Awoke Gebrie <awokegebrie5@gmail.com>
2025-01-04 01:32:21 +0300

    firebase config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)', // Matches all routes
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-store', // Disable caching for all routes
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
