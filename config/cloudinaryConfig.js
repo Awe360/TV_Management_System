75c1c8640bd9a8a0ef2e4caffcd77078f5df49dc
Awoke Gebrie <awokegebrie5@gmail.com>
2025-01-09 00:51:30 +0300

    cloudinary fix

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 // Set up Cloudinary with your cloud name, API key, and secret
 cloudinary.config({
   cloud_name: 'dtinrmkcf',
-  api_key: `${process.env.CLOUDINARY_API_KEY}`,
+  api_key: '582496718486238',
   api_secret: 'A6ch0xk3o9dY7N8AfbCNFYduDwg'
 });
 
